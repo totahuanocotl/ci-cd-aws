50c17abcca2ede64bb2a9629d6ffc7c3b7ce492f
Isaac Martinez Aceves <isaac.martinez.aceves@gmail.com>
2019-12-11 22:19:42 +0000

    Add kubeconfig

--- resources/templates/scripts/kubectl_apply.sh
+++ resources/templates/scripts/kubectl_apply.sh
@@ -5,5 +5,5 @@ DIR=$(dirname ${BASH_SOURCE[0]})
 
 echo "Running for target cluster {{ i.target_name }}"
 echo ${DIR}
-kubectl --context {{ i.cluster.context }} -n {{ i.hello_world.namespace }} apply -f ../manifests/pre-deploy
-kubectl --context {{ i.cluster.context }} -n {{ i.hello_world.namespace }} apply -f ../manifests
+kubectl --kubeconfig ./kubeconfig --context {{ i.cluster.context }} -n {{ i.hello_world.namespace }} apply -f ../manifests/pre-deploy
+kubectl --kubeconfig ./kubeconfig --context {{ i.cluster.context }} -n {{ i.hello_world.namespace }} apply -f ../manifests

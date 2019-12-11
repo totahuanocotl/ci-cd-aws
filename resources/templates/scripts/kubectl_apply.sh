9896c5932d3793da736fb8900e829ac33b33944e
Shin <758937+shin-nien@users.noreply.github.com>
2019-12-11 08:05:25 +0000

    Trigger build

--- resources/templates/scripts/kubectl_apply.sh
+++ resources/templates/scripts/kubectl_apply.sh
@@ -3,7 +3,7 @@
 {% set i = inventory.parameters %}
 DIR=$(dirname ${BASH_SOURCE[0]})
 
-echo "Running for target {{ i.target_name }}"
+echo "Running for target cluster {{ i.target_name }}"
 echo ${DIR}
 kubectl --context {{ i.cluster.context }} -n {{ i.hello_world.namespace }} apply -f ../manifests/pre-deploy
 kubectl --context {{ i.cluster.context }} -n {{ i.hello_world.namespace }} apply -f ../manifests
